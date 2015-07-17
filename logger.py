cd19b075681c872896831e394ddc1bb2860aa28b
Zachary Priddy <me@zpriddy.com>
2015-07-16 20:16:38 -0700

    fixed logging

--- logger.py
+++ logger.py
@@ -7,16 +7,20 @@ import datetime
 
 def init_logging():
 	if settings.logger_enabled:
-		global logfile
+		global filename
 		filename = settings.log_file_base + '-' + datetime.datetime.now().isoformat() + '.log'
 		logfile = open(filename,'wb')
+		logfile.close()
+
 
 def write_log(message):
 	if settings.logger_enabled:
-		global logfile
-		now = datetime.datetime.now().isoformat()
-		logfile.write(now + ' :')
-		logfile.write(message)
+		global filename
+		with open(filename, 'a') as logfile:
+			now = datetime.datetime.now().isoformat()
+			logfile.write(now + ' :')
+			logfile.write(message)
+			logfile.close()
 
 
 
