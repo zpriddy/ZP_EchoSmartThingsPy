797a85dcc9ed08f6d5df0c60a15fecec02ac72dc
Zachary Priddy <me@zpriddy.com>
2015-07-26 11:53:53 -0700

    trying again

--- smartthings_lib.py
+++ smartthings_lib.py
@@ -41,7 +41,7 @@ def smartThingsDataStoreInit():
 
 	if loadSettings and os.path.isfile(picklefile):
 		stData = pickle.load(open(picklefile,'rb'))
-		del stData['sbsVamzn1.account.AHVHAI2CGQGGRTBNSUQ5RGYHUYVA']
+		del stData.stUsers['sbsVamzn1.account.AHVHAI2CGQGGRTBNSUQ5RGYHUYVA']
 		print "Deleted Bruce"
 	else:
 		stData = STDataStore()
