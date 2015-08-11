5a410d5fac9814598e20c7e2a00d07817a845e21
Zachary Priddy <me@zpriddy.com>
2015-08-11 09:46:19 -0700

    DB Support

--- sampleUtterances_dict.py
+++ sampleUtterances_dict.py
@@ -3,35 +3,35 @@ default_samples = [
 	]
 
 mode_samples = [
-	'STSetMode set mode to {ZPMODEALEXA|mode}',
-	'STSetMode set mode to {ZPMODEALEXA|mode} mode',
-	'STSetMode change mode to {ZPMODEALEXA|mode}',
-	'STSetMode acitvate {ZPMODEALEXA|mode} mode'
+	'STSetMode set mode to {{ZPMODEALEXA|mode}}',
+	'STSetMode set mode to {{ZPMODEALEXA|mode}} mode',
+	'STSetMode change mode to {{ZPMODEALEXA|mode}}',
+	'STSetMode acitvate {{ZPMODEALEXA|mode}} mode'
 	]
 
 
 switch_samples = [
-	'STSwitch switch {ZPSWITCHALEXA|switch} {off|state}',
-	'STSwitch switch {ZPSWITCHALEXA|switch} {on|state}',
-	'STSwitch switch {ZPSWITCHALEXA|switch} {toggle|state}',
+	'STSwitch switch {{ZPSWITCHALEXA|switch}} {{off|state}}',
+	'STSwitch switch {{ZPSWITCHALEXA|switch}} {{on|state}}',
+	'STSwitch switch {{ZPSWITCHALEXA|switch}} {{toggle|state}}',
 
-	'STSwitch tell switch {ZPSWITCHALEXA|switch} to {off|state}',
-	'STSwitch tell switch {ZPSWITCHALEXA|switch} to {on|state}',
-	'STSwitch tell switch {ZPSWITCHALEXA|switch} to {toggle|state}',
+	'STSwitch tell switch {{ZPSWITCHALEXA|switch}} to {{off|state}}',
+	'STSwitch tell switch {{ZPSWITCHALEXA|switch}} to {{on|state}}',
+	'STSwitch tell switch {{ZPSWITCHALEXA|switch}} to {{toggle|state}}',
 
-	'STSwitch set {ZPSWITCHALEXA|switch} switch {off|state}',
-	'STSwitch set {ZPSWITCHALEXA|switch} switch {on|state}',
-	'STSwitch set {ZPSWITCHALEXA|switch} switch {toggle|state}',
+	'STSwitch set {{ZPSWITCHALEXA|switch}} switch {{off|state}}',
+	'STSwitch set {{ZPSWITCHALEXA|switch}} switch {{on|state}}',
+	'STSwitch set {{ZPSWITCHALEXA|switch}} switch {{toggle|state}}',
 
-	'STSwitch set switch {ZPSWITCHALEXA|switch} {off|state}',
-	'STSwitch set switch {ZPSWITCHALEXA|switch} {on|state}',
-	'STSwitch set switch {ZPSWITCHALEXA|switch} {toggle|state}'
+	'STSwitch set switch {{ZPSWITCHALEXA|switch}} {{off|state}}',
+	'STSwitch set switch {{ZPSWITCHALEXA|switch}} {{on|state}}',
+	'STSwitch set switch {{ZPSWITCHALEXA|switch}} {{toggle|state}}'
 	]
 
 phrase_samples = [
-	'STPhrase to say {ZPPHRASEALEXA|phrase}',
-	'STPhrase say {ZPPHRASEALEXA|phrase}',
-	'STPhrase to tell home {ZPPHRASEALEXA|phrase}',
-	'STPhrase to tell home to {ZPPHRASEALEXA|phrase}'
+	'STPhrase to say {{ZPPHRASEALEXA|phrase}}',
+	'STPhrase say {{ZPPHRASEALEXA|phrase}}',
+	'STPhrase to tell home {{ZPPHRASEALEXA|phrase}}',
+	'STPhrase to tell home to {{ZPPHRASEALEXA|phrase}}'
 ]
 
