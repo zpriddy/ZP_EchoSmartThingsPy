3382b8793ea697ee5061dddf06fbaeb4eaddd68f
Zachary Priddy <me@zpriddy.com>
2015-08-12 18:25:12 -0700

    More

--- echopy_doc.py
+++ echopy_doc.py
@@ -276,144 +276,32 @@ nest_page='''
 '''
 
 
-samples_page='''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-  <script>
-  (function(i,s,o,g,r,a,m){{i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){{
-  (i[r].q=i[r].q||[]).push(arguments)}},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  }})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-65257509-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
-
-</head>
-
-
-
-
-
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="https://alexa.zpriddy.com">ZP Alexa Projects</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="https://alexa.zpriddy.com">Home</a></li>
-        <li class="active"><a href="#">SmartThings<span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Nest</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="https://zpriddy.com">zpriddy.com</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+samples_page_body='''
 <div class="container">
-
-
-
-
-<form action="samples" method="post">
-<div class="form-group">
-	<label for="AlexaID">Alexa ID</label>
-	<input id="AlexaID" name="AlexaID" type="text" class="form-control" title="Alexa ID. This is a required field">
-</div>
-<input type="submit" value="Get Samples" class="btn btn-default">
-</form>
+	<form action="samples" method="post">
+		<div class="form-group">
+			<label for="AlexaID">Alexa ID</label>
+			<input id="AlexaID" name="AlexaID" type="text" class="form-control" title="Alexa ID. This is a required field">
+		</div>
+	<input type="submit" value="Get Samples" class="btn btn-default">
+	</form>
 </div>
 '''
 
-samples_results='''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-  <script>
-  (function(i,s,o,g,r,a,m){{i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){{
-  (i[r].q=i[r].q||[]).push(arguments)}},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  }})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-65257509-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
-</head>
-
-
-
-
-
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="https://alexa.zpriddy.com">ZP Alexa Projects</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="https://alexa.zpriddy.com">Home</a></li>
-        <li class="active"><a href="#">SmartThings<span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Nest</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="https://zpriddy.com">zpriddy.com</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+samples_results_body='''
 <div class="container">
-<h1>Sample Resultss</h1>
-<p> Please copy and paste the results below into your Alexa SampleUtterances in the ASK portal. </p>
-<div class="panel panel-primary">
-        <div class="panel-heading">Sample</div>
-              <div class="panel-body" style="max-height: 10;overflow-y: scroll;">RESULTS</div>
-        </div>
-  </div>
-
+	<h1>Sample Resultss</h1>
+	<p> Please copy and paste the results below into your Alexa SampleUtterances in the ASK portal. </p>
+	<div class="panel panel-primary">
+		<div class="panel-heading">Sample</div>
+		<div class="panel-body" style="max-height: 10;overflow-y: scroll;">RESULTS</div>
+	</div>
 </div>
 '''
 
 
 html_privacy_policy_body='''
+<div class="container">
 <p><strong>alexa.zpriddy.com Privacy Policy</strong>
 This privacy policy has been compiled to better serve those who are concerned with how their &#8216;Personally identifiable information&#8217; (PII) is being used online. PII, as used in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.</p>
 
@@ -543,6 +431,7 @@ and we will promptly remove you from ALL correspondence.</p>
 alexa@zpriddy.com</p>
 
 <p>Last Edited on 2015&#8211;08&#8211;11</p>
+</div>
 '''
 
 
@@ -553,5 +442,7 @@ alexa@zpriddy.com</p>
 
 main_page = page_generator('Home',main_page_body)
 auth_page = page_generator('Auth',auth_page_body)
+samples_page = page_generator('Request Samples',samples_page_body)
+samples_results = page_generator('Sample Results',samples_results_body)
 
 NotNestUser = {"outputSpeech": {"type":"PlainText","text":"Current user is not a valid nest user. Please look for help"},"card":{"type":"Simple","title":"Nest Control Error","content":"Current user is not a valid nest user. Please look for help"},'shouldEndSession':True}
