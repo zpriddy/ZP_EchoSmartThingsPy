738e3bf8783fe84f2c5f3aada37d9a23dd62c042
Zachary Priddy <me@zpriddy.com>
2015-08-15 11:01:21 -0700

    paypal button

--- echopy_doc.py
+++ echopy_doc.py
@@ -124,6 +124,19 @@ main_page_body='''
 	    	<p>Please consider helping me out so that I can keep supporting this and other Open Source projects! I run all of this out of my pocket and it doesnt all come free.. Please consider helping me out so that I can keep everything running!
 	    	</p>
 	    	<p><a class="btn btn-primary btn-lg" href="https://cash.me/$ZPriddy" role="button">Donate via Square Cash!</a></p>
+	    	<p>
+	    		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+				<input type="hidden" name="cmd" value="_donations">
+				<input type="hidden" name="business" value="paypal@zpriddy.com">
+				<input type="hidden" name="lc" value="US">
+				<input type="hidden" name="item_name" value="ZPriddy Alexa Projects">
+				<input type="hidden" name="currency_code" value="USD">
+				<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHosted">
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				</form>
+			<p>
+
 		</div>
 	<div class="col-md-2"></div>
 </div>
