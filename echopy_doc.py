9328cd3a10004a4de6d76344036cec9742169c1e
Zachary Priddy <me@zpriddy.com>
2015-08-15 11:11:28 -0700

    dfs

--- echopy_doc.py
+++ echopy_doc.py
@@ -189,7 +189,7 @@ auth_page_body='''
 					<input type="hidden" name="no_note" value="0">
 					<input type="hidden" name="currency_code" value="USD">
 					<input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_LG.gif:NonHostedGuest">
-					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" class="btn btn-default">
 					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 					</form>
 				</p>
