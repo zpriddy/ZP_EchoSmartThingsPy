34edd53dfda6d5d67c90dfc9ee30f183b86b5a47
Zachary Priddy <me@zpriddy.com>
2015-08-15 01:04:18 -0700

    Added Button on samples page

--- echopy_doc.py
+++ echopy_doc.py
@@ -312,6 +312,11 @@ nest_page='''
 
 samples_page_body='''
 <div class="container">
+	<div class="alert alert-danger alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		<p><strong>Please consider supporting this project!</strong> Donations can be made in any amount via Square Cash with no fees! </p> 
+		<a class="btn btn-success" href="https://cash.me/$ZPriddy" >Support this project on Square Cash</a>
+	</div>
 	<form action="samples" method="post">
 		<div class="form-group">
 			<label for="AlexaID">Alexa ID</label>
