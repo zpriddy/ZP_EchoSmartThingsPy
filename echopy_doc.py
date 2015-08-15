5910e8a16b0b96811ca2d7a56771c09df8fffd57
Zachary Priddy <me@zpriddy.com>
2015-08-15 11:07:44 -0700

    More

--- echopy_doc.py
+++ echopy_doc.py
@@ -173,23 +173,24 @@ auth_page_body='''
 		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 		<p><strong>Please consider supporting this project!</strong> Donations can be made in any amount via Square Cash with no fees! </p> 
 		<table>
-		<td>
-		<a class="btn btn-success" href="https://cash.me/$ZPriddy" >Support this project on Square Cash</a>
-		</td>
-		<td>
-		<p>
-    		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-			<input type="hidden" name="cmd" value="_donations">
-			<input type="hidden" name="business" value="paypal@zpriddy.com">
-			<input type="hidden" name="lc" value="US">
-			<input type="hidden" name="item_name" value="ZPriddy Alexa Projects">
-			<input type="hidden" name="currency_code" value="USD">
-			<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHosted">
-			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-			<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-			</form>
-		</p
-		</td>
+			<td>
+				<a class="btn btn-success" href="https://cash.me/$ZPriddy" >Support this project on Square Cash</a>
+			</td>
+			<td>
+				<p>
+					<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+					<input type="hidden" name="cmd" value="_donations">
+					<input type="hidden" name="business" value="paypal@zpriddy.com">
+					<input type="hidden" name="lc" value="US">
+					<input type="hidden" name="item_name" value="ZPriddy Alexa Projects">
+					<input type="hidden" name="no_note" value="0">
+					<input type="hidden" name="currency_code" value="USD">
+					<input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_LG.gif:NonHostedGuest">
+					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+					</form>
+				</p>
+			</td>
 		</table>
 	</div>
 	<form action="auth" method="post">
