431b339cb62ba6f89caae821eaa237aa5af87184
Zachary Priddy <me@zpriddy.com>
2015-08-15 14:28:05 -0700

    More Errors

--- nest_lib.py
+++ nest_lib.py
@@ -302,6 +302,23 @@ class NestThermostats:
 		return self._thermostats
 
 
+
+
+def generateError(errorMessage, functionName):
+	output_speech = "Nest encountered and error. " + errorMessage + ". Please try again"
+	output_type = "PlainText"
+
+	card_type = "Simple"
+	card_title = "Nest Error - " + functionName
+	card_content = "Smart Things encountered and error. " + errorMessage
+
+	response = {"outputSpeech": {"type":output_type,"text":output_speech},"card":{"type":card_type,"title":card_title,"content":card_content},'shouldEndSession':False}
+
+	print response
+	return response
+
+
+
 ###############################################################################
 # TO BE DELETED
 ###############################################################################
